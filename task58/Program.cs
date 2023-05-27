5ccc5fbfccdba084b64ccc96dca992a7643c6aea
Anton_Yushin <ya.napishimne@yandex.ru>
2023-05-27 06:29:53 +0300

    task58

---
+++ task58/Program.cs
@@ -0,0 +1,72 @@
+﻿//Задача 58: Задайте две матрицы. 
+//Напишите программу, которая будет находить произведение двух матриц.
+
+﻿void FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void MultMatrix(int[,] firstMatr, int[,] secondMatr, int[,] resultMatr)
+{
+    for (int i = 0; i < resultMatr.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultMatr.GetLength(1); j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < firstMatr.GetLength(1); k++)
+            {
+                sum += firstMatr[i, k] * secondMatr[k, j];
+            }
+            resultMatr[i, j] = sum;
+        }
+    }
+}
+
+Console.Write("Введите количество строк 1 массива: ");
+int rowsFirstMatrix = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов 1 массива: ");
+int columnsFirstMatrix = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество строк 2 массива: ");
+int rowsSecondMatrix = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов 2 массива: ");
+int columnsSecondMatrix = Convert.ToInt32(Console.ReadLine());
+
+if (columnsFirstMatrix != rowsSecondMatrix)
+{
+    Console.WriteLine("Такие матирицы перемножать нельзя!");
+    return;
+}
+
+int[,] firstMatrix = new int[rowsFirstMatrix, columnsFirstMatrix];
+FillArray(firstMatrix);
+Console.WriteLine($"Первая матрица:");
+PrintArray(firstMatrix);
+
+int[,] secondMatrix = new int[rowsSecondMatrix, columnsSecondMatrix];
+FillArray(secondMatrix);
+Console.WriteLine($"Вторая матрица:");
+PrintArray(secondMatrix);
+
+int[,] resultMatrix = new int[rowsFirstMatrix, columnsSecondMatrix];
+
+MultMatrix(firstMatrix, secondMatrix, resultMatrix);
+Console.WriteLine($"Произведение первой и второй матриц:");
+PrintArray(resultMatrix);
